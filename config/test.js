2f53be7ad4b68b5bbd1cde0b7af6a3d64cb01f5d
Erlend Lind Madsen <erlendf80@gmail.com>
2021-08-23 18:30:35 +0200

    config: oh, i should include the config/test.js file

---
+++ config/test.js
@@ -0,0 +1,12 @@
+module.exports = {
+	port: 4000,
+	bucket: 'images',
+	s3: {
+		region: 'us-east-1',
+		endpoint: 'http://localhost:4566',
+		s3ForcePathStyle: true
+	},
+	sizes: [[84, 84], [172, 172], [320, 320], [640, 640], [1024, 1024]],
+	keep_original: false,
+}
+
